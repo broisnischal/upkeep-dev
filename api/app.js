3968034e3be43dc748f66aed4d1b7ff0a5b0747e
Nischal Dahal <neeswebservice@gmail.com>
2023-02-03 06:25:51 +0545

    fixed the api error

--- api/app.js
+++ api/app.js
@@ -4,8 +4,8 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import 'dotenv/config';
 import * as url from 'url';
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
+import indexRouter from './src/v1/routes/index.js';
+import usersRouter from './src/v1/routes/users.js';
 
 const app = express();
 export const __filename = url.fileURLToPath(import.meta.url);
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './src/v1/public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
