e4b990f4d3aba3bd7c36ed01324cf520868f9f46
neeswebservices <neeswebservice@gmail.com>
2023-02-23 07:22:41 +0545

    merged_to_master

--- api/src/v1/models/order.model.js
+++ api/src/v1/models/order.model.js
@@ -1,38 +1,32 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
 const OrderSchema = new mongoose.Schema(
-	{
-		userId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-		amount: Number,
-		status: {
-			type: Number,
-			default: 0, // 0 - pending, 1 - approved, 3 - delivered
-		},
-		items: [
-			{
-				service: {
-					type: mongoose.Schema.Types.ObjectId,
-					ref: "Service",
-					required: true,
-				},
-				unit: { type: Number, required: true },
-			},
-		],
-	},
-	{
-		toJSON: {
-			// transform(doc, ret) {
-			// 	delete ret.__v
-			// },
-		},
-		timestamps: true,
-	},
-)
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        services: [{ sid: mongoose.Schema.Types.ObjectId }],
+        total: {
+            type: Number,
+            required: true,
+            min: [200, 'Too few Price!'],
+        },
+        status: {
+            type: String,
+            enum: {
+                values: ['PENDING', 'RESOLVED'],
+                message: 'Value error, Invalid Request !',
+            },
+            default: 'PENDING',
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
 
-const Order = mongoose.model("Order", OrderSchema)
+const Order = mongoose.model('Order', OrderSchema);
 
-export default Order
+export default Order;
